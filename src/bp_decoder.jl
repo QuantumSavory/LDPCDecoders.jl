996fc4b214fa0e6ccae40d5434933213e1b12d11
Krishna Praneet Gudipaty <kgudipaty@umass.edu>
2023-06-26 15:36:52 -0400

    Refractored decoder and added tests

--- src/bp_decoder.jl
+++ src/bp_decoder.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 
 
 # Bi-Symmetric Channel LLR (Bit flip)
-function llr(error_rate, bit)
+function llr(bit, error_rate)
   return ((-1) ^ bit) * log((1-error_rate)/error_rate)
 end
 
@@ -25,29 +25,7 @@ function estimate(message_c2v, num_vs, num_cs, vllr, parity_check_matrix)
   return estimate
 end
 
-# Belief Propagation Decoder
-function bp_decode(received_message, parity_check_matrix, error_rate, max_iterations=100)
-  
-  num_checks, num_bits = size(parity_check_matrix)
-  num_edges = sum(parity_check_matrix)
-
-  num_cs, num_vs = size(parity_check_matrix)
-  
-  # Initialize messages
-  message_c2v = zeros(num_checks, num_bits)
-  message_v2c = zeros(num_checks, num_bits)
-
-
-  # Messages received by the variables 
-  
-  ## For every variable node, there should be initialised llr
-  vllr = zeros(num_bits)
-  for i in 1:num_bits
-    vllr[i] = llr(error_rate, received_message[i])
-  end 
-  
-
-  # Initial message received by check nodes
+function send_variable_message!(parity_check_matrix, message_c2v, message_v2c, num_cs, num_vs, vllr)
   for check in 1:num_cs
     for v in 1:num_vs
       if parity_check_matrix[check, v] == 1
@@ -63,57 +41,68 @@ function bp_decode(received_message, parity_check_matrix, error_rate, max_iterat
       end
     end  
   end
+end
+
+function send_check_message!(parity_check_matrix, message_c2v, message_v2c, num_cs, num_vs)
+  for variable in 1:num_vs 
+    for c in 1:num_cs
+      if parity_check_matrix[c, variable] == 1 
+
+        sum = 0
+        sgn = 1
+        for v in 1:num_vs 
+          if parity_check_matrix[c, v] == 1 && v != variable 
+            sgn *= sign(message_v2c[c, v])
+            sum += phi(abs(message_v2c[c,v]))
+          end
+        end
+        message_c2v[c, variable] = sgn * phi(sum)
+      end
+    end
+  end
+end
+
+
+# Belief Propagation Decoder
+function bp_decode(parity_check_matrix, received_message, error_rate, max_iterations=100)
+  
+  num_checks, num_bits = size(parity_check_matrix)
+  num_edges = sum(parity_check_matrix)
+
+  num_cs, num_vs = size(parity_check_matrix)
+  
+  # Initialize messages
+  message_c2v = zeros(num_checks, num_bits)
+  message_v2c = zeros(num_checks, num_bits)
+
+
+  # Intialize llr for variable nodes
+  vllr = llr.(received_message, error_rate)  
+
+  # Send message from variable to check nodes
+  send_variable_message!(parity_check_matrix, message_c2v, message_v2c, num_checks, num_bits, vllr)
 
   local decoded
   local syndrome
 
   for iter in 1:max_iterations
     
-    # Message receive by variable nodes
-    for variable in 1:num_vs 
-      for c in 1:num_cs
-        if parity_check_matrix[c, variable] == 1 
+    # Send Message from check to variable nodes
+    send_check_message!(parity_check_matrix, message_c2v, message_v2c, num_checks, num_bits)
 
-          sum = 0
-          sgn = 1
-          for v in 1:num_vs 
-            if parity_check_matrix[c, v] == 1 && v != variable 
-              sgn *= sign(message_v2c[c, v])
-              sum += phi(abs(message_v2c[c,v]))
-            end
-          end
-          message_c2v[c, variable] = sgn * phi(sum)
-        end
-      end
-    end
+    # Send message from variable to check nodes
+    send_variable_message!(parity_check_matrix, message_c2v, message_v2c, num_checks, num_bits, vllr)
 
-    # Message receive by check nodes
-    for check in 1:num_cs
-      for v in 1:num_vs
-        if parity_check_matrix[check, v] == 1
-          sum = 0
-        
-          for c in 1:num_cs
-            if c != check && parity_check_matrix[c, v] == 1
-              sum += message_c2v[c, v]  
-            end
-          end
-  
-          message_v2c[check, v] = sum + vllr[v]  
-        end
-      end  
-    end
-
-
+    # Estimate LLR and calculate Syndrome to check H.c+e = 0
     llr = estimate(message_c2v, num_vs, num_cs, vllr, parity_check_matrix)
     decoded = llr .< 0
     syndrome = (parity_check_matrix * decoded) .% 2
-    
+
+    # Check if decoded is a codeword
     if iszero(syndrome)
       break
     end
 
-    
   end
 
   return decoded, iszero(syndrome)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,31 @@ using LDPC
 using Test
 
 @testset "LDPC.jl" begin
-    # Write your tests here.
+    
+    error_rate = 0.1
+    
+    parity_check_matrix = [1 0 1 0 1 0 1; 0 1 1 0 0 1 1; 0 0 0 1 1 1 1]
+    actual_message = [1 1 1 1 1 1 1]
+    received_message = [1 1 1 1 1 0 1]  # 2nd bit from right is an error
+    decoded_message, success = bp_decode(parity_check_matrix, received_message, error_rate)
+    
+    actual_message = vec(actual_message)
+    @test decoded_message == actual_message
+
+    parity_check_matrix = [1 0 1 1 1 0 0; 1 1 0 1 0 1 0; 1 1 1 0 0 0 1]
+    actual_message = [1 0 0 0 1 1 1]
+    received_message = [1 0 0 1 1 1 1] # 4th bit from left is an error
+    decoded_message, success = bp_decode(parity_check_matrix, received_message, error_rate)
+    
+    actual_message = vec(actual_message)
+    @test decoded_message == actual_message
+    
+    parity_check_matrix = [1 1 1 1 0 0 0 0 0 0; 1 0 0 0 1 1 1 0 0 0; 0 1 0 0 1 0 0 1 1 0; 0 0 1 0 0 1 0 1 0 1; 0 0 0 1 0 0 1 0 1 1]
+    actual_message = [1 1 0 0 1 0 0 0 0 0]
+    received_message = [1 1 0 0 1 0 0 0 0 1] # last bit is an error
+    decoded_message, success = bp_decode(parity_check_matrix, received_message, error_rate)
+    
+    actual_message = vec(actual_message)
+    @test decoded_message == actual_message
+
 end
