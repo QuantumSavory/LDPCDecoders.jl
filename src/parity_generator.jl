4ad4c414ca6a77f0ae1541ecc24f2cae3ce92d61
Krishna Praneet Gudipaty <kgudipaty@umass.edu>
2023-07-24 18:49:47 -0400

    Added BP syndrome decoder and parity generator

--- src/parity_generator.jl
+++ src/parity_generator.jl
@@ -1,21 +1,46 @@
 using Random
 
-function parity_check_matrix(n, rw, cw)
-  n_equations = (n  * cw) ÷ rw
+"""
+parity_check_matrix(n, wr, wc)
+
+Computes a regular LDPC matrix using Callager's algorithm
+
+Block is created initially and then the columns are randomly
+permuted to create additional parity check n_equations
+
+# Arguments
+* `n`: The length of the code
+* `wr`: Row weight (Number of bits a parity check equations acts upon). Must divide n
+* `wc`: Column weight (Number of parity check equations for a given bit)
+
+# Examples
+```julia
+julia> using LDPC
+julia> H = parity_check_matrix(1000, 10, 9)
+```
+"""
+function parity_check_matrix(n::Int, wr::Int, wc::Int)
   
-  block_size = n_equations ÷ rw
-  block = zeros(Int64, block_size, n)
+  # For a regular LDPC matrix
+  ## wr = wc * (n / n-k)
+  @assert n % wr == 0
   
+  n_equations = (n  * wc) ÷ wr
+  block_size = n_equations ÷ wc
+
+  block = zeros(Bool, block_size, n)
   
   for i in 1:block_size
-    for j in ((i-1)*cw + 1):((i)*cw)
+    for j in ((i-1)*wr + 1):((i)*wr)
       block[i,j] = 1
     end
   end
+
   H = block
 
-  for i in 1:rw - 1
+  for i in 1:wc - 1
     H = [H; block[:, shuffle(1:end)]] 
   end
+
   return H
 end
---
+++ src/syndrome_decoder.jl
@@ -0,0 +1,92 @@
+function syndrome_decode(pcm, syndrome, error_rate, max_iters)
+  
+  # Get size of Parity check matrix
+  m, n = size(pcm)
+
+  # Initialise channel probabilities
+  channel_probs = fill(error_rate, n)
+  
+  # Initialise messages between checks, bits array
+  # 3rd dimension: 1 -> bit to check, 2-> check to bit
+  msg = zeros(m, n, 2)
+
+  # Initialize log probabilities
+  log_probabs = zeros(n)
+
+  # Initiliase bit to check messages
+  for j in 1:n
+    for i in 1:m
+      if pcm[i,j] == 1
+        msg[i, j, 1] = (channel_probs[j]) / (1 - channel_probs[j])
+      end
+    end
+  end
+
+  error = zeros(Bool,n)
+  converged = 0
+  for iter in 1:max_iters
+
+    # Check to bit messages
+    for i in 1:m
+      temp = (-1) ^ syndrome[i]
+      for j in 1:n
+        if pcm[i,j] == 1
+          msg[i,j,2] = temp
+          temp *= 2 / (1 + msg[i,j,1]) - 1
+        end
+      end
+
+      temp = 1.0
+      for j=n:-1:1
+        if pcm[i,j] == 1
+          msg[i,j,2] *= temp
+          msg[i,j,2] = (1 - msg[i,j,2]) / (1 + msg[i,j,2])
+          temp *= 2/(1 + msg[i,j,1]) - 1
+        end
+      end
+    end
+
+    # display(msg)
+    # Bit to check messages
+    for j in 1:n
+      temp = channel_probs[j] / (1 - channel_probs[j])
+
+      for i in 1:m
+        if pcm[i,j] == 1
+          msg[i,j,1] = temp
+          temp *= msg[i,j,2]
+          if isnan(temp)
+            temp = 1.0
+          end
+        end
+      end
+
+      log_probabs[j] = log(1 / temp)
+      if temp >= 1
+        error[j] = 1
+      else 
+        error[j] = 0
+      end
+
+      temp = 1.0
+      for i=m:-1:1
+        if pcm[i,j] == 1
+          msg[i,j,1] *= temp
+          temp *= msg[i,j,2]
+          if isnan(temp)
+            temp = 1.0
+          end
+        end
+      end
+    end
+
+    syndrome_decoded = (pcm * error) .% 2
+    if all(syndrome_decoded .== syndrome)
+      converged = true
+      return error, converged
+    end
+  end
+  return Int.(error), converged
+
+
+end
