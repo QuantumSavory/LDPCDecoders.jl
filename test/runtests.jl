e19cced8c65520767d1c44899d00bff68dbb7ce3
Krishna Praneet Gudipaty <kgudipaty@umass.edu>
2023-07-25 16:27:59 -0400

    Added iterative syndrome decoder

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,19 +10,25 @@ using Test
     error = [0 0 0 0 0 1 0]
     syn = (pcm * error') .% 2
     decoded_error, success = syndrome_decode(pcm, syn, error_rate, 100)
+    decoded_error_it, success = syndrome_it_decode(pcm, syn, 100)
     @test decoded_error == vec(error)
+    @test decoded_error_it == vec(error)
 
     pcm = [1 0 1 1 1 0 0; 1 1 0 1 0 1 0; 1 1 1 0 0 0 1]
     error = [0 0 0 1 0 0 0]
     syn = (pcm * error') .% 2
     decoded_error, success = syndrome_decode(pcm, syn, error_rate, 100)
+    decoded_error_it, success = syndrome_it_decode(pcm, syn, 100)
     @test decoded_error == vec(error)
+    @test decoded_error_it == vec(error)
     
     pcm = [1 1 1 1 0 0 0 0 0 0; 1 0 0 0 1 1 1 0 0 0; 0 1 0 0 1 0 0 1 1 0; 0 0 1 0 0 1 0 1 0 1; 0 0 0 1 0 0 1 0 1 1]
     error = [0 0 0 0 0 0 0 0 0 1] # last bit is an error
     syn = (pcm * error') .% 2
     decoded_error, success = syndrome_decode(pcm, syn, error_rate, 100)
+    decoded_error_it, success = syndrome_it_decode(pcm, syn, 100)
     @test decoded_error == vec(error)
+    @test decoded_error_it == vec(error)
     
 
     ## Parity check matrix generation tests
