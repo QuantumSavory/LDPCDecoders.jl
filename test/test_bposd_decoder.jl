8fc81f5a9c8953ecc845930d84ce4276fc42a315
Yuxuan Yan <yyan@duck.com>
2024-11-09 14:52:14 +0100

    add basic tests

---
+++ test/test_bposd_decoder.jl
@@ -0,0 +1,34 @@
+using Test
+using LDPCDecoders
+
+@testset "test_bposd_decoder.jl" begin
+
+  """Test for BP-OSD decoder"""
+  function test_bposd_decoder()
+    H = LDPCDecoders.parity_check_matrix(1000, 10, 9)
+    per = 0.01
+    err = rand(1000) .< per
+    syn = (H * err) .% 2
+
+    bposd = BeliefPropagationOSDDecoder(H, per, 100)
+    guess, success = decode!(bposd, syn)
+
+    return guess == err
+  end
+
+  """Test"""
+  function test_bposd_decoder_large_error_rate()
+    H = LDPCDecoders.parity_check_matrix(1000, 10, 9)
+    per = 0.2
+    err = rand(1000) .< per
+    syn = (H * err) .% 2
+
+    bposd = BeliefPropagationOSDDecoder(H, per, 100)
+    guess, success = decode!(bposd, syn)
+
+    return syn == (H * guess) .% 2
+  end
+
+  @test test_bp_decoder()
+  @test test_bposd_decoder_large_error_rate()
+end
