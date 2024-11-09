cf29b76afa95408f1ccfda7b4a140db851481d1d
Yuxuan Yan <yyan.tsinghua@gmail.com>
2024-11-09 22:34:52 +0800

    add high osd_order tests

--- test/test_bposd_decoder.jl
+++ test/test_bposd_decoder.jl
@@ -16,6 +16,24 @@ using LDPCDecoders
     return guess == err
   end
 
+  """Test high order OSD"""
+  function test_bposd_decoder_high_order()
+    H = LDPCDecoders.parity_check_matrix(1000, 10, 9)
+    per = 0.01
+    err = rand(1000) .< per
+    syn = (H * err) .% 2
+
+    orders = 2:5
+    succ = true
+    for osd_order in orders
+      bposd = BeliefPropagationOSDDecoder(H, per, 100; osd_order=osd_order)
+      guess, success = decode!(bposd, syn)
+      succ = succ & (guess == err)
+    end
+
+    return succ
+  end
+
   """Test for BP-OSD decoder with large error rate. Even if the decoding is not accurate, OSD will still ensure consistency between guess and syndromes."""
   function test_bposd_decoder_large_error_rate()
     H = LDPCDecoders.parity_check_matrix(1000, 10, 9)
@@ -30,5 +48,6 @@ using LDPCDecoders
   end
 
   @test test_bposd_decoder()
+  @test test_bposd_decoder_high_order()
   @test test_bposd_decoder_large_error_rate()
 end
