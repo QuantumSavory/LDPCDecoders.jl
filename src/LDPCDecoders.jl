7022cdcc598d2fd38948bcd2658641ff50a47e06
the-punisher-29 <soumenkumar9503@gmail.com>
2026-07-05 16:42:34 +0530

    Remove legacy standalone decoders and simulators
    Remove 6 legacy files that are superseded by the modern struct-based
    decoders in src/decoders/:
    
    Standalone decoders (not exported, not used by any modern code):
      - src/bp_decoder.jl: old bp_decode(), superseded by decoders/belief_propagation.jl
      - src/it_decoder.jl: old it_decode(), superseded by decoders/iterative_bitflip.jl
      - src/bitflip_noise.jl: add_bitflip_noise(), never referenced anywhere
    
    Simulators (call the above deleted decoders):
      - src/bp_simulator.jl: bp_simulate(), has debug println/display calls
      - src/syndrome_simulator.jl: syndrome_simulate(), calls syndrome_decode()
      - src/syndrome_it_simulate.jl: syndrome_it_simulate(), calls syndrome_it_decode()

--- src/LDPCDecoders.jl
+++ src/LDPCDecoders.jl
@@ -18,9 +18,6 @@ export
     BPOTSDecoder
 
 include("generator.jl")
-include("bp_decoder.jl")
-include("bp_simulator.jl")
-include("it_decoder.jl")
 include("parity_generator.jl")
 
 include("decoders/abstract_decoder.jl")
@@ -30,9 +27,7 @@ include("decoders/iterative_bitflip.jl")
 include("decoders/bpots_decoder.jl")
 
 include("syndrome_bp_decoder.jl")
-include("syndrome_simulator.jl")
 include("syndrome_it_decoder.jl")
-include("syndrome_it_simulate.jl")
 
 
 end
